5a82ba98320b7cedf82102a6cb16f17729d5dbb9
Miguel <miguel.danto@gmail.com>
2022-01-30 00:36:12 -0600

    temporal preview

---
+++ preview/script.js
@@ -0,0 +1,4 @@
+import { GeodatasetsMap } from "../src/index.js";
+
+let mapa1 = new GeodatasetsMap({target:"mapa-1"})
+console.log(mapa1)
---
+++ preview/webpack.preview.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './script.js',
+    output:{
+        filename: '_bundle.js',
+        path: path.resolve(__dirname,''),
+        libraryTarget: 'umd',
+        library: {
+            name: "geods",
+            type: 'var',
+            //export: 'default'
+        }
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js','.scss','.css']
+    },
+    module:{
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                  MiniCssExtractPlugin.loader,
+                  {
+                    loader: 'css-loader',
+                    options:{
+                        url:false
+                    }
+                  },
+                  {
+                    loader: 'sass-loader',
+                    options: {
+                      sourceMap: false
+                    }
+                  }
+        
+                ]
+              }
+        
+        ]
+    },
+    
+    
+    plugins:[
+        new MiniCssExtractPlugin({
+            filename:"_bundle.css"
+        }),
+        
+        new  CopyPlugin({
+            patterns: [
+              {from:"src/styles/fonts",to:"fonts"},
+            ]
+        }),
+        new HtmlWebpackPlugin({
+            template: "preview/template.html",
+            filename: '_preview.html'
+        }),
+    ]
+    
+    
+}
